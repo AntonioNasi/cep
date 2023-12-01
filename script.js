a06cd1d0f1ac256e08e1c6a177b06328619b202d
Toim Nasi <86308977+AntonioNasi@users.noreply.github.com>
2023-11-30 22:24:17 -0300

    NewCep

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 /* Ler mais */
+function LerMais3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Ler mais"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Ocultar texto";
+        moreText.style.display = "inline";
+    }
+}
+
 function LerMais2() {
     var dots = document.getElementById("dots2");
     var moreText = document.getElementById("more2");
